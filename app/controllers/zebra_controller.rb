30f9630dd7533899793a774cf62ca0bf5c292c72
Brian Song <brian.cs.song@gmail.com>
2024-10-30 05:37:36 +0000

    Save for rock

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -2,10 +2,27 @@ class ZebraController < ApplicationController
   def giraffe
     @random_move = ["rock", "paper", "scissors"].sample
     
+    if @comp_move == "rock"
+      @outcome = "tied"
+    elsif @comp_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+
     render({ :template => "game_templates/play_rock" })
   end
 
   def elephant
     # ...
+  end
+
+  def lion
+
+  end
+
+  def rules
+
+    render({ :template => "game_templates/play_rock" })
   end
 end
